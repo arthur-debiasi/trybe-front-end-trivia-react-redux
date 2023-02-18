2bba5bcdc9ab2a43fc2dcb2273dedb72eaf68a9a
arthur-debiasi <arthurdebiasi@hotmail.com>
2023-02-18 17:33:01 -0300

    feat: req 19

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -2,13 +2,29 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 class Ranking extends Component {
+  state = {
+    ranking: JSON.parse(localStorage.getItem('ranking')),
+  };
+
   render() {
+    const { ranking } = this.state;
     return (
       <div>
         <h1 data-testid="ranking-title">Ranking</h1>
         <Link to="/">
           <button type="button" data-testid="btn-go-home">Home</button>
         </Link>
+        <div>
+          {ranking.map(({ name, score }, i) => (
+            <li key={ name }>
+              <span data-testid={ `player-name-${i}` }>{name}</span>
+              {' '}
+              - Score:
+              {' '}
+              <span data-testid={ `player-score-${i}` }>{score}</span>
+            </li>
+          ))}
+        </div>
       </div>
     );
   }
