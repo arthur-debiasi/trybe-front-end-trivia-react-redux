9c376c545a98f4585531826f4090ae7307fbf559
arthur-debiasi <arthurdebiasi@hotmail.com>
2023-02-18 18:49:18 -0300

    feat: req 20

--- src/tests/Rankings.test.js
+++ src/tests/Rankings.test.js
@@ -1,16 +1,48 @@
-// import React from 'react';
-// import { screen } from '@testing-library/react';
-// import userEvent from '@testing-library/user-event';
-// import App from '../App';
-// import renderWithRouterAndRedux from './helpers/renderWithRouterAndRedux';
-// import Ranking from '../pages/Ranking';
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouterAndRedux from './helpers/renderWithRouterAndRedux';
+import Ranking from '../pages/Ranking';
+import { RankingMock } from './helpers/localStorageMock';
 
-// describe('Testa a página Login', () => {
-//   test('Verifica se existem dois botões play e settings', () => {
-//     renderWithRouterAndRedux(<Ranking />);
-//     const title = screen.getByRole('heading', { name: /ranking/i });
-//     const homeBtn = screen.getByRole('button', { name: /home/i })
-//     expect(title).toBeInTheDocument();
-//     expect(homeBtn).toBeInTheDocument();
-//   });
-// });
+const localStorageMock = (function () {
+  let store = {};
+
+  return {
+    getItem(key) {
+      return store[key];
+    },
+
+    setItem(key, value) {
+      store[key] = value;
+    },
+
+    clear() {
+      store = {};
+    },
+
+    removeItem(key) {
+      delete store[key];
+    },
+
+    getAll() {
+      return store;
+    },
+  };
+})();
+
+Object.defineProperty(window, "localStorage", { value: localStorageMock }); 
+// achei isto em https://robertmarshall.dev/blog/how-to-mock-local-storage-in-jest-tests/
+// eu ainda não consigo entender o define property, mas entendo o funcionamento do localStorageMock
+
+describe('Testa a página Login', () => {
+  test('Verifica se existem título e botão', () => {
+    localStorageMock.setItem('ranking', RankingMock);
+    renderWithRouterAndRedux(<Ranking />);
+    const title = screen.getByRole('heading', { name: /ranking/i });
+    const homeBtn = screen.getByRole('button', { name: /home/i });
+    expect(title).toBeInTheDocument();
+    expect(homeBtn).toBeInTheDocument();
+  });
+});
---
+++ src/tests/helpers/localStorageMock.js
@@ -0,0 +1,13 @@
+const ranking = [{
+  name: 'Carlos Marcos',
+  gravatarEmail: 'carlosmarcos@email.com',
+  score: 210
+},
+{
+  name: 'Adão Ferreira',
+  gravatarEmail: 'adaoferreira@email.com',
+  score: 210
+}]
+const RankingMock = JSON.stringify(ranking)
+
+export { RankingMock }
